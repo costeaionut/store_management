f0b7cda65249a78c27740ecada10e07f0c645158
Costea Ionut-Andrei <costeacionut@gmail.com>
2024-12-07 18:26:31 +0200

    Update UserRepository with email fetch and create UserDetailsService

--- src/main/java/com/ing/demo/store_management/repository/UserRepository.java
+++ src/main/java/com/ing/demo/store_management/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.ing.demo.store_management.model.authentication.StoreUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<StoreUser, Integer> {
-
+    Optional<StoreUser> findByEmail(String email);
 }
