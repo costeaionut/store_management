9081ed634b99b294b2f15a6614481aa2390c9aea
Costea Ionut-Andrei <costeacionut@gmail.com>
2024-12-10 21:30:17 +0200

    Create basic input sanitizer

---
+++ src/main/java/com/ing/demo/store_management/service/impl/BasicInputSanitizer.java
@@ -0,0 +1,32 @@
+package com.ing.demo.store_management.service.impl;
+
+import com.ing.demo.store_management.service.InputSanitizer;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class BasicInputSanitizer implements InputSanitizer {
+
+    private static final Map<String, String> ESCAPING_RULES = new HashMap<>();
+
+    static {
+        ESCAPING_RULES.put("'", "\\'");
+        ESCAPING_RULES.put("\"", "\\\"");
+        ESCAPING_RULES.put(";", "\\;");
+        ESCAPING_RULES.put("\\", "\\\\");
+        ESCAPING_RULES.put("%", "\\%");
+        ESCAPING_RULES.put("_", "\\_");
+    }
+    @Override
+    public String sanitizeInput(String rawInput) {
+        String escapedInput = rawInput;
+
+        for (Map.Entry<String, String> entry : ESCAPING_RULES.entrySet()) {
+            escapedInput = escapedInput.replace(entry.getKey(), entry.getValue());
+        }
+
+        return escapedInput;
+    }
+}
