f2d4ef8dd482b32bc02de6111a48e04db8ea415f
Costea Ionut-Andrei <costeacionut@gmail.com>
2024-12-12 01:07:30 +0200

    Add tests and javadoc

--- src/main/java/com/ing/demo/store_management/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ing/demo/store_management/service/impl/ProductServiceImpl.java
@@ -26,11 +26,20 @@ public class ProductServiceImpl implements ProductService {
         this.repository = repository;
     }
 
+    /**
+     * Retrieve all products in the repository
+     * @return a list of all the products
+     */
     @Override
     public List<Product> retrieveAllProducts() {
         return repository.findAll();
     }
 
+    /**
+     * Retrieve a product based on the id
+     * @param id product id
+     * @return the product with the specified id
+     */
     @Override
     public Product retrieveProductById(int id) {
         Optional<Product> product = repository.findById(id);
@@ -40,6 +49,11 @@ public class ProductServiceImpl implements ProductService {
         return product.get();
     }
 
+    /**
+     * Add a new product in the database
+     * @param newProduct add the product
+     * @return the created product
+     */
     @Override
     public Product createProduct(@Validated Product newProduct) {
         if (newProduct == null) {
@@ -48,6 +62,12 @@ public class ProductServiceImpl implements ProductService {
         return repository.save(newProduct);
     }
 
+    /**
+     * Update a product based on the id and the new values
+     * @param id product id
+     * @param updatedProduct product new info
+     * @return the updated product
+     */
     @Override
     public Product updateProduct(int id, @Validated Product updatedProduct) {
         if (updatedProduct == null) {
@@ -61,11 +81,21 @@ public class ProductServiceImpl implements ProductService {
         return existingProduct;
     }
 
+    /**
+     * Delete a product based on id
+     * @param id product id
+     */
     @Override
     public void deleteProduct(int id) {
         repository.deleteById(id);
     }
 
+    /**
+     * Retrieves the appropriate mapper based on the category of product
+     * @param category product category
+     * @return the product mapper
+     */
+    @Override
     public ProductMapper<?> getMapperFromCategory(Category category) {
         switch (category) {
             case GROCERY -> {
